77f5f33a9d30600fd868d768f6b740db40301634
pohl989 <pohl989@gmail.comm>
2017-11-19 12:19:27 -0700

    Add profiles and seeds

--- db/migrate/20171021235254_create_profiles.rb
+++ db/migrate/20171021235254_create_profiles.rb
@@ -2,9 +2,35 @@ class CreateProfiles < ActiveRecord::Migration[5.1]
   def change
     create_table :profiles do |t|
       t.string :first_name
+      t.string :middle_name
       t.string :last_name
-      t.string :title
-      t.string :email
+      t.string :primary_phone
+      t.string :alternate_phone
+      t.string :personal_email
+      t.string :work_email
+      t.string :preferred_name
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :zip_code
+      t.string :gender
+      t.string :country_of_origin
+      t.string :primary_language
+      t.string :english_proficiency
+      t.string :employment_status
+      t.string :employment_type
+      t.boolean :discriminated_at_work
+      t.boolean :health_insurance
+      t.boolean :medical_home
+      t.string :medical_home_location
+      t.boolean :primary_physician
+      t.integer :household_size
+      t.integer :children_under_18
+      t.string :household_income
+      t.boolean :registered_to_vote
+      t.date :date_of_birth
+      t.boolean :text_messages
+
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,65 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Let the seeding begin!"
+
+User.create! ([
+  email: "pohl989@gmail.com",
+  password: "password",
+  password_confirmation: "password"])
+
+puts "Added Ben's account"
+# emails = ["pohl989@gmail.com", "luis@cuutah.org"]
+# emails.each do |email|
+#   user = User.new(email: email, password: "password", password_confirmation: "password", role: :admin, verified: true)
+#   puts "Setting up user, contact record for : #{user}.email"
+#   user.save
+# end
+# puts "Added Admin Users"
+#
+
+#adding variables
+primary_language = ["English", "Spanish", "English and Spanish", "Other"]
+country = ["United States", "Mexico", "Peru", "Cuba", "Spain", "Chile", "Panama", "Germany"]
+english_proficiency = ["Low", "Intermediate", "High"]
+gender = ["Male", "Female", "Other"]
+employment_status = ["Employed", "Unemployed"]
+employment_type = ["Employed for Wages", "Self Employed", "Out of Work and looking", "Out of Work and NOT looking", "Homemaker", "Student", "Military", "Retired", "Unable to Work"]
+medical_home_location = ["Community Clinic", "Mobile Clinic", "University of Utah", "Intermountian", "Private Clinic"]
+household_income = ["Less than $10,000", "$10,000 to $20,000", "$20,000 to $30,000", "$30,000 to $40,000", "more than $40,000"]
+
+100.times do
+  Profile.create! ([
+    first_name: Faker::StarTrek.character,
+    middle_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    preferred_name: Faker::Name.first_name,
+    street: Faker::Address.street_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr,
+    zip_code: Faker::Address.zip_code,
+    primary_phone: Faker::Base.numerify('(###) ###-####'),
+    alternate_phone: Faker::Base.numerify('(###) ###-####'),
+    personal_email: Faker::Internet.email,
+    work_email: Faker::Internet.email,
+    gender: gender,
+    country_of_origin: country.sample,
+    primary_language: primary_language.sample,
+    english_proficiency: english_proficiency,
+    employment_status: employment_status.sample,
+    employment_type: employment_type.sample,
+    discriminated_at_work: Faker::Boolean.boolean(0.2),
+    health_insurance: Faker::Boolean.boolean(0.8),
+    medical_home: Faker::Boolean.boolean(0.6),
+    medical_home_location: medical_home_location,
+    primary_physician: Faker::Boolean.boolean(0.8),
+    household_size: Faker::Number.between(1, 10),
+    children_under_18: Faker::Number.between(1, 8),
+    household_income: household_income,
+    registered_to_vote: Faker::Boolean.boolean(0.6),
+    date_of_birth: Faker::Date.birthday(18, 65),
+    text_messages: Faker::Boolean.boolean(0.8),
+    ])
+end
+
+puts "Added Profiles"
